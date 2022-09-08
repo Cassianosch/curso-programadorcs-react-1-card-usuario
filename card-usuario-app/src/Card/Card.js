99fe618f71c60073e57c6177519b8cde198d769f
Cassiano S <cassiano@scheidemantel.com.br>
2022-09-07 21:17:55 -0300

    fix: adicionado estrutura do projeto

---
+++ card-usuario-app/src/Card/Card.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./estilo.scss";
+
+const Card = () => {
+  return (
+    <div className="App">
+      <h1>Programador.cs</h1>
+    </div>
+  );
+};
+
+export default Card;
--- card-usuario-app/src/index.js
+++ card-usuario-app/src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import Card from "./Card";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Card />
   </React.StrictMode>
 );
